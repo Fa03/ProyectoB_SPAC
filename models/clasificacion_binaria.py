215e48ad60384733319442bda172b6c4a4b538d6
Fabian Brenes <fabianbrenes@ice.co.cr>
2026-06-14 13:33:42 -0600

    feature:ya guarda los.csv en la carpeta de "data/processed"

--- models/clasificacion_binaria.py
+++ models/clasificacion_binaria.py
@@ -8,7 +8,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report,confusion_matrix
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense,Dropout
-
+from pathlib import Path
 
 class ClasificacionBinaria:
 
@@ -24,6 +24,12 @@ class ClasificacionBinaria:
         Y = df['Churn_Yes'].values
 
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=101)
+        df_Train = pd.DataFrame(X_train)
+        df_Test = pd.DataFrame(X_test)
+        # Guardar datos de entrenamiento en la carpeta "data/processed"
+        carpeta = Path.cwd().parent / 'data' / 'processed'
+        df_Train.to_csv(carpeta/'train_binario.csv', index=False)
+        df_Test.to_csv(carpeta/'test_binario.csv', index=False)
 
         return X,Y, X_train, X_test, y_train, y_test
 
@@ -63,7 +69,21 @@ class ClasificacionBinaria:
         print(confusion_matrix(y_test, predictions))
 
     def guardar_modelo(self,model, nombre_modelo):
-        model.save(nombre_modelo + ".h5")
+        model.save(nombre_modelo + ".keras")
+
+    def prediccion(self, model):
+        # Lectura datos a predecir
+        df_pred = pd.read_csv('C:/Users/fab_t/Downloads/telco_churn_generado_1000.csv')
+        # df_pred = df_pred[
+        #     df.columns]  # Filtramos el dataframe predicción para quedarnos con las mismas columnas de interés que el df histórico
+        X_pred = df_pred.drop('Churn_Yes',
+                              axis=1)  # Eliminamos la columna objetivo (vacía) si la hubiera en nuestro df con los datos a predecir
+        #X_pred = scaler.transform(X_pred)
+        # Realizamos predicción
+        resultado = (model.predict(X_pred) > 0.5).astype("int32")
+        # Unimos en un dataframe los datos a predecir con su predicción
+        df_pred['PREDICCIÓN'] = pd.DataFrame(resultado)
+        print(df_pred)
 
 
 prueba_binaria = ClasificacionBinaria()
@@ -72,7 +92,7 @@ modelo_creado = prueba_binaria.crearModeloBinario(X,Y, X_train, X_test, y_train,
 prueba_binaria.evaluar_modelo(X_test, y_test, modelo_creado)
 prueba_binaria.guardar_modelo(modelo_creado,"modelo_TELCO")
 
-
+prueba_binaria.prediccion(modelo_creado)
 
 
 
